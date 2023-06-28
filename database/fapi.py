fde32dae7eb8e8f0df862e0df6e3cb335faa83ee
fantazzzt <102978932+fantazzzt@users.noreply.github.com>
2023-06-27 20:17:33 -0500

    fastapi usage on the coindb
    allows website to call on the database to use

---
+++ database/fapi.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+import sqlite3
+
+app = FastAPI()
+
+@app.get("/coin/{coin_id}")
+def get_coin(coin_id: int):
+    conn = sqlite3.connect('coin.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT cgID, cg_num_coins, cg_mint, cg_start_year, Location_ID FROM coin_table WHERE cgID = ?", (coin_id,))
+    result = cursor.fetchone()
+    conn.close()
+    
+    if result is None:
+        return {"error": "Coin not found"}
+
+    coin_info = {
+        "cgID": result[0],
+        "cg_num_coins": result[1],
+        "cg_mint": result[2],
+        "cg_start_year": result[3],
+        "Location_ID": result[4],
+    }
+    
+    return coin_info
