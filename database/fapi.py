659d5bccadbe4afea1ed557790653d5bc50892bf
fantazzzt <102978932+fantazzzt@users.noreply.github.com>
2023-07-03 18:25:36 -0500

    Update fapi.py to flask
    Will use a javascript library to make a histogram from the JSONS

--- database/fapi.py
+++ database/fapi.py
@@ -1,49 +1,21 @@
-from fastapi import FastAPI, Request
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-import matplotlib.pyplot as plt
-from io import BytesIO
-import sqlite3
-import requests
+from flask import Flask, jsonify
+from flask_cors import CORS
 
-app = FastAPI()
-templates = Jinja2Templates(directory="templates")
+app = Flask(__name__)
+CORS(app)
 
-
-@app.get("/")
-def index(request: Request):
-    return templates.TemplateResponse("economy.html", {"request": request})
-
-@app.get("/generate_histogram")
-def generate_histogram_route():
-    sample_size = 100
-    coin_mints = get_random_coin_sample(sample_size)
-
-    plt.hist(coin_mints, bins=10)
-    plt.xlabel("cg_mint")
-    plt.ylabel("Frequency")
-    plt.title("Histogram of cg_mint")
-
-    plt.savefig("static/histogram.png")
-    plt.close()
-
-    return "Histogram generated successfully!"
+@app.route('/')
+def index():
+    coin_mints = get_random_coin_sample(sample_size=10)
+    return jsonify(coin_mints)
 
 def get_random_coin_sample(sample_size):
-    response = requests.get(f"http://localhost:8000/coin?sample_size={sample_size}")
-    data = response.json()
-    coin_mints = []
-    for coin in data:
-        if coin.get("cg_mint"):
-            coin_mints.append(coin["cg_mint"])
+    coin_mints = [
+        {"cg_mint": "Mint 1"},
+        {"cg_mint": "Mint 2"},
+        {"cg_mint": "Mint 3"}
+    ]
     return coin_mints
 
-
-def generate_histogram(sample_size):
-    coin_mints = get_random_coin_sample(sample_size)
-
-    plt.hist(coin_mints, bins=10)
-    plt.xlabel("cg_mint")
-    plt.ylabel("Frequency")
-    plt.title("Histogram of cg_mint")
-    plt.show()
+if __name__ == '__main__':
+    app.run()
