edfb66e013b539b5755c384c9e90bd2fcdc92d10
fantazzzt <102978932+fantazzzt@users.noreply.github.com>
2023-07-06 14:49:57 -0500

    Update fapi.py to provide example data for chart

--- database/fapi.py
+++ database/fapi.py
@@ -1,20 +1,25 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+import sqlite3
+
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
 def index():
-    coin_mints = get_random_coin_sample(sample_size=10)
+    coin_mints = get_random_coin_sample(sample_size=100)
     return jsonify(coin_mints)
 
+
 def get_random_coin_sample(sample_size):
-    coin_mints = [
-        {"cg_mint": "Mint 1"},
-        {"cg_mint": "Mint 2"},
-        {"cg_mint": "Mint 3"}
-    ]
+    conn = sqlite3.connect('C:/Users/masse/aoe2stuff/historyweb/database/coin.db', check_same_thread=False)
+    cursor = conn.cursor()
+    query = "SELECT cg_mint FROM Coingroups LIMIT " + str(sample_size)
+    cursor.execute(query)
+    coin_mints = cursor.fetchall()
+    coin_mints = [coin for coin in coin_mints if coin[0]]  
+    conn.close()
     return coin_mints
 
 if __name__ == '__main__':
